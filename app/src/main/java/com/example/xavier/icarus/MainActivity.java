0cff744b78dce202b63c6c9998b5a4ac5757dc12
DaniloXavier08 <daniloxavier97@hotmail.com>
2019-01-31 16:56:28 -0200

    Open File Dialog: bug fixed; Open File Dialog: open only MIDI files;

--- app/src/main/java/com/example/xavier/icarus/MainActivity.java
+++ app/src/main/java/com/example/xavier/icarus/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.xavier.icarus;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
@@ -33,11 +36,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Set Toolbar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         // Floating Action button
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,10 +53,13 @@ public class MainActivity extends AppCompatActivity {
         mediaPlayer = MediaPlayer.create(this, R.raw.happy_birthday);
 
         // Button stuff
-        Button buttonPlay = (Button) findViewById(R.id.play);
-        Button buttonPause = (Button) findViewById(R.id.pause);
+        Button buttonPlay = findViewById(R.id.play);
+        Button buttonPause = findViewById(R.id.pause);
         buttonPlay.setOnClickListener(buttonPlayOnClickListener);
         buttonPause.setOnClickListener(buttonPauseOnClickListener);
+
+        Button buttonOpen = findViewById(R.id.open);
+        buttonOpen.setOnClickListener(buttonOpenOnClickListener);
 
     }
 
@@ -84,8 +90,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             if (!mediaPlayer.isPlaying()) {
-                mediaPlayer.start();
-                Toast.makeText(MainActivity.this, "mediaPlayer.star", Toast.LENGTH_SHORT).show();
+                try{
+                    mediaPlayer.start();
+                    Toast.makeText(MainActivity.this, "mediaPlayer.start()", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
+                }
             }
         }
     };
@@ -100,43 +110,52 @@ public class MainActivity extends AppCompatActivity {
     };
 
 
-    private void showFiles() {
-        try {
-
-            List<String> minhaLista;
-            minhaLista = new ArrayList<String>();
-
-            String path;
-            File sdCardRoot = Environment.getExternalStorageDirectory();
-            File dir = new File(sdCardRoot.getAbsolutePath() + "/Test/");
-
-            Toast.makeText(this, sdCardRoot.toString(), Toast.LENGTH_LONG).show();
-
-            if (dir.exists()) {
-
-                if (dir.listFiles() != null) {
-                    for (File f : dir.listFiles()) {
-                        if (f.isFile()) {
-                            path = f.getName();
-                            minhaLista.add(path);
-                        }
-
-                        //if (path.contains(".mid")) {
-
-                        //}
-                    }
-                }
-            }
-
-            ListView listView = (ListView) findViewById(R.id.lista_arquivos);
-
-            listView.setAdapter(new ArrayAdapter<String>(this,
-                    android.R.layout.simple_list_item_1, minhaLista));
-
-        } catch (Exception e) {
-            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+    Button.OnClickListener buttonOpenOnClickListener = new Button.OnClickListener() {
+        @Override
+        public void onClick(View v){
+            openMidiFile();
         }
+    };
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+
+        // The ACTION_OPEN_DOCUMENT intent was sent with the request code
+        // READ_REQUEST_CODE. If the request code seen here doesn't match, it's the
+        // response to some other intent, and the code below shouldn't run at all.
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // The document selected by the user won't be returned in the intent.
+            // Instead, a URI to that document will be contained in the return intent
+            // provided to this method as a parameter.
+            // Pull that URI using resultData.getData().
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+                Toast.makeText(MainActivity.this, resultData.toString(), Toast.LENGTH_LONG).show();
+                mediaPlayer = MediaPlayer.create(this, uri);
+            }
+        }
+    }
+
+
+    private static final int READ_REQUEST_CODE = 42;
+
+    protected void openMidiFile() {
+        // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
+        // browser.
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+
+        // Filter to only show results that can be "opened", such as a
+        // file (as opposed to a list of contacts or timezones)
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        // Filter to show only images, using the image MIME data type.
+        // If one wanted to search for ogg vorbis files, the type would be "audio/ogg".
+        // To search for all documents available via installed storage providers,
+        // it would be "*/*".
+        intent.setType("audio/midi");
+
+        startActivityForResult(intent, READ_REQUEST_CODE);
     }
 
 }
