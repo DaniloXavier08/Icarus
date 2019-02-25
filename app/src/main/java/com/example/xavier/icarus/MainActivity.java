ea14c8f34b327748e13211223261e7f227de7538
DaniloXavier08 <daniloxavier97@gmail.com>
2019-02-24 22:31:53 -0300

    Add Midi Sheet Music Library.

--- app/src/main/java/com/example/xavier/icarus/MainActivity.java
+++ app/src/main/java/com/example/xavier/icarus/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.xavier.icarus;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -14,14 +12,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
